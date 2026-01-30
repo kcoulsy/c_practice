24102debe3cda44b1f88e2f436055fc745647d4e
Kristian Coulson <kristiancoulson@gmail.com>
2026-01-30 07:44:06 +0000

    C super basics

--- basics/src/main.c
+++ basics/src/main.c
@@ -1,4 +1,6 @@
+#include <inttypes.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void) {
     printf("Hello, World!\n");
@@ -15,9 +17,88 @@ int main(void) {
     printf("char c = %c\n", c);
     printf("double d = %lf\n", d);
 
-    // print newline
     printf("\n");
 
+    printf("Input an integer: ");
+    int x;
+
+    printf("int x = %d\n", x); // printing before assignment
+    scanf_s("%d", &x); // <- &x is a pointer to the memory location of x
+
+    // Input an integer: int x = 32759
+    // sd
+    // int x = 32759
+    // x is positive
+    // it didn't break when non int was inputted but it did fail
+    // it returned 0 items successfully read and so x was never assigned
+    // it printed whatever was in memory at the address of x
+
+    printf("int x = %d\n", x);
+
+    // if else statements simple enough
+    if (x > 0) {
+        printf("x is positive\n");
+    } else if (x < 0) {
+        printf("x is negative\n");
+    } else {
+        printf("x is zero\n");
+    }
+
+    printf("\n");
+
+    // same with switch
+    switch(x) {
+        case 0:
+            printf("x is zero\n");
+            break;
+        case 1:
+            printf("x is one\n");
+            break;
+        default:
+            printf("x is not zero or one\n");
+            break;
+    }
+
+    printf("\n");
+
+    // for loop
+    // for (int i = 0; i < x; i++) {
+    //     printf("%d ", i);
+    // }
+
+    printf("\n");
+
+    // while loops
+    // int y = 0;
+    // while (y < 10) {
+    //     printf("%d ", y);
+    //     y++;
+    // }
+
+    printf("\n");
+
+    int first = 4;
+    int second = 10;
+    // arithmatic
+    int add = first + second;
+    int subtract = first - second;
+    int multiply = first * second;
+    int divide = first / second;
+    int modulus = first % second;
+
+    printf("add = %d\n", add);
+    printf("subtract = %d\n", subtract);
+    printf("multiply = %d\n", multiply);
+    printf("divide = %d\n", divide);
+    printf("modulus = %d\n", modulus);
+
+    // logical operators
+    bool contained = x > 0 && x < 10;
+    bool either = x == 0 || x == 1;
+    bool neither = !(x == 0 || x == 1);
+
+    // ternary operator
+    int result = x > 0 ? x : -x;
 
     return 0;
 }
