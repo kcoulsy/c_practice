082221cc239200b38dc8b641338d78e02201ac7c
Kristian Coulson <kristiancoulson@gmail.com>
2026-01-30 07:30:41 +0000

    Init Repo

---
+++ basics/src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+
+    // variables
+    int a = 10;
+    float b = 3.14; // 32 bit float
+    char c = 'A';
+    double d = 2.718; // 64 bit double
+
+    // print variables
+    printf("int a = %d\n", a);
+    printf("float b = %f\n", b);
+    printf("char c = %c\n", c);
+    printf("double d = %lf\n", d);
+
+    // print newline
+    printf("\n");
+
+
+    return 0;
+}
